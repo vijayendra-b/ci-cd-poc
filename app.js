8a6d28c6d85e30458405b3500fb4493614409dee
vijayendra-b <vijayendraswamy@gmail.com>
2026-01-16 12:31:46 +0530

    updated app.js file

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello CI/CD POC 3 on 15th Jan 2025'));
+app.get('/', (req, res) => res.send('Hello CI/CD POC 3 on 16th Jan 2025 for the Demo'));
 
 app.listen(3000, () => console.log('App running on port 3000'));
